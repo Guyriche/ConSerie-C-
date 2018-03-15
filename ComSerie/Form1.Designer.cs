3b913527f9049c499ba923cb3cee083949826074
Guyriche <guysselnindjeu@gmail.com>
2018-03-15 10:58:14 +0100

    Communinication port serie en c#

---
+++ ComSerie/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace ComSerie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbSendData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CbAfficheur = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnenvoi = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnlecture = new System.Windows.Forms.Button();
+            this.tbreception = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.cbDatabits = new System.Windows.Forms.ComboBox();
+            this.cbstopbits = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbSendData
+            // 
+            this.tbSendData.Enabled = false;
+            this.tbSendData.Location = new System.Drawing.Point(3, 19);
+            this.tbSendData.Multiline = true;
+            this.tbSendData.Name = "tbSendData";
+            this.tbSendData.Size = new System.Drawing.Size(379, 66);
+            this.tbSendData.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BAUDS";
+            // 
+            // CbAfficheur
+            // 
+            this.CbAfficheur.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbAfficheur.FormattingEnabled = true;
+            this.CbAfficheur.Location = new System.Drawing.Point(135, 19);
+            this.CbAfficheur.Name = "CbAfficheur";
+            this.CbAfficheur.Size = new System.Drawing.Size(121, 21);
+            this.CbAfficheur.TabIndex = 5;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.cboBaudRate.Location = new System.Drawing.Point(135, 46);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudRate.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnenvoi);
+            this.groupBox1.Controls.Add(this.tbSendData);
+            this.groupBox1.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(34, 165);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 120);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Partie d\'Envoie";
+            // 
+            // btnenvoi
+            // 
+            this.btnenvoi.Enabled = false;
+            this.btnenvoi.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenvoi.Location = new System.Drawing.Point(158, 91);
+            this.btnenvoi.Name = "btnenvoi";
+            this.btnenvoi.Size = new System.Drawing.Size(75, 23);
+            this.btnenvoi.TabIndex = 2;
+            this.btnenvoi.Text = "Envoie";
+            this.btnenvoi.UseVisualStyleBackColor = true;
+            this.btnenvoi.Click += new System.EventHandler(this.btnenvoi_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnlecture);
+            this.groupBox2.Controls.Add(this.tbreception);
+            this.groupBox2.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(434, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(329, 120);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Partie Reception";
+            // 
+            // btnlecture
+            // 
+            this.btnlecture.Enabled = false;
+            this.btnlecture.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlecture.Location = new System.Drawing.Point(122, 91);
+            this.btnlecture.Name = "btnlecture";
+            this.btnlecture.Size = new System.Drawing.Size(75, 23);
+            this.btnlecture.TabIndex = 2;
+            this.btnlecture.Text = "Lecture";
+            this.btnlecture.UseVisualStyleBackColor = true;
+            this.btnlecture.Click += new System.EventHandler(this.btnlecture_Click);
+            // 
+            // tbreception
+            // 
+            this.tbreception.Enabled = false;
+            this.tbreception.Location = new System.Drawing.Point(3, 19);
+            this.tbreception.Multiline = true;
+            this.tbreception.Name = "tbreception";
+            this.tbreception.ReadOnly = true;
+            this.tbreception.Size = new System.Drawing.Size(318, 66);
+            this.tbreception.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(434, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(329, 48);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "Ouverture duPort";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(434, 117);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(329, 42);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Fermeture";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(434, 73);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(329, 21);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(286, 19);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 38);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // cbParity
+            // 
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Items.AddRange(new object[] {
+            "Even",
+            "Odd",
+            "None",
+            "Mark",
+            "Space"});
+            this.cbParity.Location = new System.Drawing.Point(135, 73);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(121, 21);
+            this.cbParity.TabIndex = 13;
+            // 
+            // cbDatabits
+            // 
+            this.cbDatabits.FormattingEnabled = true;
+            this.cbDatabits.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cbDatabits.Location = new System.Drawing.Point(135, 100);
+            this.cbDatabits.Name = "cbDatabits";
+            this.cbDatabits.Size = new System.Drawing.Size(121, 21);
+            this.cbDatabits.TabIndex = 14;
+            // 
+            // cbstopbits
+            // 
+            this.cbstopbits.FormattingEnabled = true;
+            this.cbstopbits.Items.AddRange(new object[] {
+            "None",
+            "One",
+            "OnePointFive",
+            "Two"});
+            this.cbstopbits.Location = new System.Drawing.Point(135, 127);
+            this.cbstopbits.Name = "cbstopbits";
+            this.cbstopbits.Size = new System.Drawing.Size(121, 21);
+            this.cbstopbits.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "PARITY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "STOPbITS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "DATABITS";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(286, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Quitter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(775, 295);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbstopbits);
+            this.Controls.Add(this.cbDatabits);
+            this.Controls.Add(this.cbParity);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.CbAfficheur);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Communication Serie";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbSendData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CbAfficheur;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnenvoi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnlecture;
+        private System.Windows.Forms.TextBox tbreception;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.ComboBox cbDatabits;
+        private System.Windows.Forms.ComboBox cbstopbits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ComSerie/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace ComSerie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getPort();
+        }
+        void getPort()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            CbAfficheur.Items.AddRange(ports);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // ici on va gérer les exception pour la securité du programme
+            try
+            {
+                if(CbAfficheur.Text == "" || cboBaudRate.Text =="" /* || cbParity.Text=="" 
+                    ||cbDatabits.Text==""  ||cbstopbits.Text==""*/)
+                {
+                   
+                    MessageBox.Show("AUCUN PARAMETRE SELECTIONNER !!!");
+                }
+                else
+                {
+
+                    // permet d'afficher la valeur du port lu
+                    serialPort1.PortName = CbAfficheur.Text;
+                    // permet de convertir la valeur du baud en int
+                    serialPort1.BaudRate = Convert.ToInt32((cboBaudRate.Text));
+                    serialPort1.Parity   = (Parity)Enum.Parse(typeof(Parity), cbParity.Text);
+                    serialPort1.DataBits = Convert.ToInt32(cbDatabits.Text);
+                    serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cbstopbits.Text);
+                    //permet d'ouvrir le port selecionné
+                    serialPort1.Open();
+                    // bar je chargement d'envoie des données
+                    progressBar1.Value = 100;
+                    //if (progressBar1.Value == 100)
+                    //    MessageBox.Show("PORT OUVERT !!");
+                    //cbParity.Enabled = true;
+                    //cbDatabits.Enabled = true;
+                    //cbstopbits.Enabled = true;
+                    btnenvoi.Enabled = true;
+                    btnlecture.Enabled = true;
+                    tbSendData.Enabled = true;
+                    btnClose.Enabled = true;
+                    btnOpen.Enabled = false;
+
+
+                }
+            }
+            catch(UnauthorizedAccessException)
+            {
+                tbreception.Text = "ACCES NON AUTORISE !";
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            btnenvoi.Enabled = false;
+            btnlecture.Enabled = false;
+            btnOpen.Enabled = false;
+            btnClose.Enabled = false;
+            tbSendData.Enabled = true;
+        }
+
+        private void btnenvoi_Click(object sender, EventArgs e)
+        {
+            if (tbSendData.Text == "")
+            {
+                MessageBox.Show("AUCUN VALEUR ENTRER !!!");
+            }
+            serialPort1.WriteLine(tbSendData.Text);
+            //tbSendData.Text = "";
+        }
+
+        private void btnlecture_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbreception.Text = tbSendData.Text;
+            }
+            catch(TimeoutException)
+            {
+                tbreception.Text = "Time out Exception";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            CbAfficheur.Items.Clear();
+            cboBaudRate.Items.Clear();
+            tbSendData.Text  = "";
+            tbreception.Text = "";
+            getPort();
+            cboBaudRate.Items.Add(9600);
+            btnenvoi.Enabled = false;
+            btnlecture.Enabled = false;
+            tbSendData.Enabled = false;
+            btnClose.Enabled = false;
+            btnOpen.Enabled = true;
+        }
+    }
+}
